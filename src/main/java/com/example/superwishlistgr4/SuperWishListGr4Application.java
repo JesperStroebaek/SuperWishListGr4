6e314048287e1b210c5be23ebb96e5c38542fdcc
Jesper <jesp73g2@stud.kea.dk>
2021-04-12 13:54:12 +0200

    1st commit

---
+++ src/main/java/com/example/superwishlistgr4/SuperWishListGr4Application.java
@@ -0,0 +1,13 @@
+package com.example.superwishlistgr4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperWishListGr4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperWishListGr4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/superwishlistgr4/SuperWishListGr4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superwishlistgr4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperWishListGr4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
