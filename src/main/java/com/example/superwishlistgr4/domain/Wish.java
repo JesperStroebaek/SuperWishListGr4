ff67a69f6fa05f26f6c151ae1c52049fe2fe9291
mathslothkea <math68n7@stud.kea.dk>
2021-04-15 13:28:40 +0200

    updated User class in domain package updated Wishlist class in domain package updated Wish in domain package

--- src/main/java/com/example/superwishlistgr4/domain/Wish.java
+++ src/main/java/com/example/superwishlistgr4/domain/Wish.java
@@ -1,4 +1,67 @@
 package com.example.superwishlistgr4.domain;
 
 public class Wish {
+    private int wishID;
+    private String titel;
+    private String link;
+    private String fulfillwish;
+    private int userid;
+    private int wishlistid;
+
+    public Wish(int wishID, String titel, String link, String fulfillwish, int userid, int wishlistid) {
+        this.wishID = wishID;
+        this.titel = titel;
+        this.link = link;
+        this.fulfillwish = fulfillwish;
+        this.userid = userid;
+        this.wishlistid = wishlistid;
+    }
+
+    public int getWishID() {
+        return wishID;
+    }
+
+    public void setWishID(int wishID) {
+        this.wishID = wishID;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getFulfillwish() {
+        return fulfillwish;
+    }
+
+    public void setFulfillwish(String fulfillwish) {
+        this.fulfillwish = fulfillwish;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public int getWishlistid() {
+        return wishlistid;
+    }
+
+    public void setWishlistid(int wishlistid) {
+        this.wishlistid = wishlistid;
+    }
 }
--- src/main/java/com/example/superwishlistgr4/domain/Wishlist.java
+++ src/main/java/com/example/superwishlistgr4/domain/Wishlist.java
@@ -1,4 +1,49 @@
 package com.example.superwishlistgr4.domain;
 
+import java.sql.Date;
+
 public class Wishlist {
+    private int wishlistid;
+    private String wishtlistname;
+    private Date enddate;
+    private int userid;
+
+    public Wishlist(int wishlistid, String wishtlistname, Date enddate, int userid) {
+        this.wishlistid = wishlistid;
+        this.wishtlistname = wishtlistname;
+        this.enddate = enddate;
+        this.userid = userid;
+    }
+
+    public int getWishlistid() {
+        return wishlistid;
+    }
+
+    public void setWishlistid(int wishlistid) {
+        this.wishlistid = wishlistid;
+    }
+
+    public String getWishtlistname() {
+        return wishtlistname;
+    }
+
+    public void setWishtlistname(String wishtlistname) {
+        this.wishtlistname = wishtlistname;
+    }
+
+    public Date getEnddate() {
+        return enddate;
+    }
+
+    public void setEnddate(Date enddate) {
+        this.enddate = enddate;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
 }
