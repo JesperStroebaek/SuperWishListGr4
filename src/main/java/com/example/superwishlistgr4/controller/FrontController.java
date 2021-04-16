081b7616dbfc777b9b558e20be94a1bbdb9d08fe
Jesper <jesp73g2@stud.kea.dk>
2021-04-16 12:01:06 +0200

    html start

--- src/main/java/com/example/superwishlistgr4/controller/FrontController.java
+++ src/main/java/com/example/superwishlistgr4/controller/FrontController.java
@@ -1,11 +1,23 @@
 package com.example.superwishlistgr4.controller;
 
+import org.apache.tomcat.jni.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
 public class FrontController {
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
+    }
+
+    // postmapping sender "dit" tavleopslag videre.
+    @PostMapping("/submitform")
+    public String post(@RequestParam("username") String username, @RequestParam("password") String password)
+    {
+        //User user = new User(username,password); ------------ ???
+
+        return "redirect:/succes";
     }
 }
