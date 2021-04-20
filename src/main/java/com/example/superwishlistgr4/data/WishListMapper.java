24a5f661c76475ac7cbee66c37c8fa96277402bd
mathslothkea <math68n7@stud.kea.dk>
2021-04-20 10:19:22 +0200

    ADDED method in wishmapper

--- src/main/java/com/example/superwishlistgr4/data/WishListMapper.java
+++ src/main/java/com/example/superwishlistgr4/data/WishListMapper.java
@@ -28,6 +28,8 @@ public class WishListMapper {
         }
 
 
+
+
     }
 
     //SELECT * FROM wish_list "
--- src/main/java/com/example/superwishlistgr4/data/WishMapper.java
+++ src/main/java/com/example/superwishlistgr4/data/WishMapper.java
@@ -1,4 +1,38 @@
 package com.example.superwishlistgr4.data;
 
+import com.example.superwishlistgr4.model.SQLexceptionhandler;
+import com.example.superwishlistgr4.model.Wish;
+import java.sql.*;
+
 public class WishMapper {
+
+    public void createWish(Wish wish) throws SQLexceptionhandler {
+        try {
+            Connection con = DBManager.getConnection();
+            String SQL = "INSERT INTO Wish (/*check table navne*/wishID, titel, link, fulfillwish, userID, wishlistid) VALUES ( ?, ?, ?, ?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, wish.getWishID());
+            ps.setString(2, wish.getTitel());
+            ps.setString(3, wish.getLink());
+            ps.setString(4, wish.getFulfillwish());
+            ps.setInt(5, wish.getUserid());
+            ps.setInt(6, wish.getWishlistid());
+            ps.executeUpdate();
+
+            System.out.println("So far so good");
+
+            ResultSet ids = ps.getGeneratedKeys();
+            ids.next();
+            int id = ids.getInt(1);
+            wish.setWishID(id);
+
+            System.out.println("so far good");
+
+            System.out.println(SQL);
+            System.out.println(ps);
+        } catch (SQLException ex) {
+            throw new SQLexceptionhandler(ex.getMessage());
+        }
+    }
 }
+
